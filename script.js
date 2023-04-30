764091997d4ffc0dcbbad8eef60cf61b891a9293
Deltas Lephalala <lephalad@legal.regn.net>
2023-04-30 21:16:48 +0200

    init code - almost complete

---
+++ script.js
@@ -0,0 +1,30 @@
+const navbar = document.querySelector('.header .navbar');
+const menuButton = document.querySelector('.header .menu');
+
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
+
+document.onscroll = () => {
+  navbar.classList.remove('show');
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+document.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+document.addEventListener('click', (event) => {
+  if (!navbar.contains(event.target) && !menuButton.contains(event.target)) {
+    navbar.classList.remove('show');
+  }
+});
